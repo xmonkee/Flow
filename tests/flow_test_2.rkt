1e591863ac2f081f915f3f52dbcfa36c4c89f5b4
xmonkee <xmonkeegtd@gmail.com>
2015-01-30 17:24:17 -0600

    Better example

--- tests/flow_test_2.rkt
+++ tests/flow_test_2.rkt
@@ -7,19 +7,18 @@
 
 (define jf
   (let* ([f1 (make-flow (hash 'default (mktext "A 1")
-                              'blank (mktext "A blank")
-                              'snd (mktext "A 2")
-                              'trd (mktext "A 3")
-                              'fth (mktext "A 5")))]
+                              'Asnd (mktext "A 2")
+                              'Atrd (mktext "A 3")
+                              'Afth (mktext "A 5")))]
          [f2 (make-flow (hash 'default (mktext "B 1")
-                              'trd (mktext "B 3")))]
+                              'Btrd (mktext "B 3")))]
          [f3 (make-flow (hash 'default (mktext "A 1")
-                              'snd (mktext "C 2")
-                              'frt (mktext "C 4")
-                              'fth (mktext "C 5")))])
+                              'Csnd (mktext "C 2")
+                              'Cfrt (mktext "C 4")
+                              'Cfth (mktext "C 5")))])
                            (join-flows ht-append f1 f2 f3)))
                      
 
-(flow-slide '(default snd (blank trd) frt fth) jf)
+(flow-slide '(default (Asnd Csnd) (Atrd Btrd) Cfrt (Afth Cfth) ) jf)
 
 
