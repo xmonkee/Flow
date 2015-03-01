6e009e43be242a82f642b7377c216ef3d362f9d6
xmonkee <xmonkeegtd@gmail.com>
2015-03-01 11:58:11 -0600

    refactor

---
+++ flow/extra.rkt
@@ -0,0 +1,73 @@
+#lang racket
+(require flow/flow)
+(require slideshow)
+
+;; Make a flow with frames named ['defaul, name-2, name-3,...]
+(define (make-named-flow name pict-lst)
+  (define (newsym num)
+    (string->symbol (string-append (symbol->string name) "-" (number->string num))))
+  (let* ([names (stream-cons 'default 
+                             (stream-map newsym (in-naturals 2)))]
+         [pairs (for/list ([name names]
+                           [pct pict-lst])
+                          (cons name pct))])
+    (make-flow (make-immutable-hash pairs))))
+
+;; Make a flow that unfolds like the original 'steps parameter of slide
+(define (make-named-steps name #:join-fn (join-fn vl-append) pict-lst)
+  (define (pct-steps acc pict-lst)
+    (if (empty? pict-lst)
+      acc
+      (if (empty? acc)
+        (pct-steps (list (car pict-lst)) (cdr pict-lst))
+        (pct-steps (cons (join-fn (car acc) (car pict-lst)) acc) (cdr pict-lst)))))
+  (make-named-flow name  (reverse (pct-steps '() pict-lst))))
+
+;; Scale a flow by given factor. Scales all alts
+(define (scale-flow factor flw)
+  (flow
+    (scale (flow-canvas flw) factor)
+    (for/hash 
+      ([(key pct) (in-hash (flow-alts flw))])
+      (values key (scale pct factor))))) 
+
+;; Possible error in this. Loses picts
+(define (scale-flows factor flws)
+  (if (manyflows? flws)
+    (manyflows
+      (scale (manyflows-canvas flws) factor)
+      (for/list
+        ([flw (manyflows-children flws)])
+        (scale-flow factor flw)))
+    (scale-flows factor (promote flws))))
+
+(define (scale-factor canvas frame)
+  (let* ([cw (pict-width canvas)]
+         [ch (pict-height canvas)]
+         [fw (pict-width frame)]
+         [fh (pict-height frame)]
+         [rw (/ fw cw)]
+         [rh (/ fh ch)]
+         [cthinner (> rw 1)]
+         [cshorter (> rh 1)])
+    (cond
+      [(and cthinner cshorter) (min rw rh)]
+      [(and (not cthinner) (not cshorter)) (min rw rh)]
+      [cthinner rh]
+      [else rw])))
+
+(define (show-manyflows-scaled flws frames)
+  (let* ([factor (scale-factor (manyflows-canvas flws) titleless-page)]
+         [flws* (manyflows 
+                  (scale (manyflows-canvas flws) factor) 
+                  (map (curry scale-flow factor) (manyflows-children flws)))])
+    (show-manyflows flws* frames)))
+
+(define (show-manyflows-scaled-2 flws frames)
+  (let* ([canvas (manyflows-canvas flws)]
+         [factor (scale-factor canvas titleless-page)])
+    (for/fold ([final (scale (manyflows-canvas flws) factor)])
+              ([flw (manyflows-children flws)])
+              (let-values ([(x y) (lt-find canvas (flow-canvas flw))])
+                          (pin-over final (* factor x) (* factor y) (flow-get-pict flw frames))))))
+
--- flow/flow.rkt
+++ flow/flow.rkt
@@ -39,28 +39,6 @@
       (flow canvas alts))))
 
 
-;; Make a flow with frames named ['defaul, name-2, name-3,...]
-(define (make-named-flow name pict-lst)
-  (define (newsym num)
-    (string->symbol (string-append (symbol->string name) "-" (number->string num))))
-  (let* ([names (stream-cons 'default 
-                             (stream-map newsym (in-naturals 2)))]
-         [pairs (for/list ([name names]
-                           [pct pict-lst])
-                          (cons name pct))])
-    (make-flow (make-immutable-hash pairs))))
-
-;; Make a flow that unfolds like the original 'steps parameter of slide
-(define (make-named-steps name #:join-fn (join-fn vl-append) pict-lst)
-  (define (pct-steps acc pict-lst)
-    (if (empty? pict-lst)
-      acc
-      (if (empty? acc)
-        (pct-steps (list (car pict-lst)) (cdr pict-lst))
-        (pct-steps (cons (join-fn (car acc) (car pict-lst)) acc) (cdr pict-lst)))))
-  (make-named-flow name  (reverse (pct-steps '() pict-lst))))
-
-
 ;; Add a key-pict pair to a flow
 (define (flow-set-pict flw frame-name pct)
   (match flw [(flow canvas alts)
@@ -86,23 +64,6 @@
          [(flow . _) (flow->manyflows flw)]
          [(manyflows . _) flw]))
 
-;; Scale a flow by given factor. Scales all alts
-(define (scale-flow factor flw)
-  (flow
-    (scale (flow-canvas flw) factor)
-    (for/hash 
-      ([(key pct) (in-hash (flow-alts flw))])
-      (values key (scale pct factor))))) 
-
-;; Possible error in this. Loses picts
-(define (scale-flows factor flws)
-  (if (manyflows? flws)
-    (manyflows
-      (scale (manyflows-canvas flws) factor)
-      (for/list
-        ([flw (manyflows-children flws)])
-        (scale-flow factor flw)))
-    (scale-flows factor (promote flws))))
 
 ;; Add a pict/flow/manyflows to a pict/flow/manyflows with pict-composing functions
 ;; and return a manyflows
@@ -112,7 +73,7 @@
           (add-flow join-fn (flow->manyflows flws) flw)] 
          [(pict . _)
           (add-flow join-fn (make-flow flws) flw)]
-         [(manyflows canvas children) 
+         ((manyflows canvas children) 
           (match flw
                  [(flow canvas alts)
                   (manyflows (join-fn (manyflows-canvas flws) (flow-canvas flw))
@@ -121,7 +82,7 @@
                   (manyflows (join-fn (manyflows-canvas flws) (manyflows-canvas flw))
                              (append (manyflows-children flws) (manyflows-children flw)))]
                  [(pict . _)
-                  (add-flow join-fn flws (make-flow flw))])]))
+                  (add-flow join-fn flws (make-flow flw))]))))
 
 
 
@@ -139,34 +100,4 @@
               ([flw (manyflows-children flws)])
               (let-values ([(x y) (lt-find canvas (flow-canvas flw))])
                           (pin-over final x y (flow-get-pict flw frames))))))
-
-(define (scale-factor canvas frame)
-  (let* ([cw (pict-width canvas)]
-         [ch (pict-height canvas)]
-         [fw (pict-width frame)]
-         [fh (pict-height frame)]
-         [rw (/ fw cw)]
-         [rh (/ fh ch)]
-         [cthinner (> rw 1)]
-         [cshorter (> rh 1)])
-    (cond
-      [(and cthinner cshorter) (min rw rh)]
-      [(and (not cthinner) (not cshorter)) (min rw rh)]
-      [cthinner rh]
-      [else rw])))
-
-(define (show-manyflows-scaled flws frames)
-  (let* ([factor (scale-factor (manyflows-canvas flws) titleless-page)]
-         [flws* (manyflows 
-                  (scale (manyflows-canvas flws) factor) 
-                  (map (curry scale-flow factor) (manyflows-children flws)))])
-    (show-manyflows flws* frames)))
-
-(define (show-manyflows-scaled-2 flws frames)
-  (let* ([canvas (manyflows-canvas flws)]
-         [factor (scale-factor canvas titleless-page)])
-    (for/fold ([final (scale (manyflows-canvas flws) factor)])
-              ([flw (manyflows-children flws)])
-              (let-values ([(x y) (lt-find canvas (flow-canvas flw))])
-                          (pin-over final (* factor x) (* factor y) (flow-get-pict flw frames))))))
 
